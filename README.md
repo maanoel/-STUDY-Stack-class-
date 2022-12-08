ae327cd6b164c08ae4c146605467aba26f60b2ea
maanoel <manoelvitorbrito@gmail.com>
2022-12-08 14:43:03 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# -STUDY-Stack-class-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-cyfzer)
---
+++ index.js
@@ -0,0 +1,25 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  clear() {
+    this.items = [];
+  }
+}
